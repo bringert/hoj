6da021043b689966dd67fb98884cf7ca70c0a334
bjorn <bjorn@bringert.net>
2004-05-20 22:31:17 +0100

    more little tests
    darcs-hash:20040520213117-6cdb2-e984430969cb71c8eea46a561e00c3fa94f27c8c

--- examples/test.java
+++ examples/test.java
@@ -1,16 +1,41 @@
 import fun.*;
 import io.*;
 
+import java.util.Iterator;
+
+import static fun.Map.*;
+
 public class test {
 
-   public static void main(String[] args) {
-      Fun<Object,Integer> hash = new Fun<Object,Integer>() {
-         public Integer apply(Object o) {
-            return new Integer(o.hashCode());
-         }
-      };
+        public static void test1() {
+                Fun<Object,Integer> hash = new Fun<Object,Integer>() {
+                        public Integer apply(Object o) {
+                                return new Integer(o.hashCode());
+                        }
+                };
+                
+                hash.map(new ReadLine(System.in).repeat());
+        }
 
-      hash.map(new ReadLine(System.in).repeat());
-   }
+        public static <A> Iterator<? extends A> cast(Iterator<A> it) {
+                return it;
+        }
+
+        public static void test2(Iterator<String> it) {
+                Iterator<? extends String> io = it;
+                while (io.hasNext()) {
+                        String s = io.next();
+                }
+        }
+
+        public static void test3(){
+                Fun<Object,Boolean> isNull = null;
+                Iterator<String> is = null;
+                Iterator<Boolean> ib = map(isNull, is);
+        }
+
+        public static void main(String[] args) {
+                
+        }
 
 }
