f25439b568aa76a264f3fbfb45132ba25de0d83f
bringert <bringert@cs.chalmers.se>
2005-06-21 18:57:25 +0100

    Implement remove() in LazyMap
    darcs-hash:20050621175725-ea354-f07db1bf194f51ee49089058b8c173d4eb06dec3

--- examples/Radix.java
+++ examples/Radix.java
@@ -3,8 +3,22 @@ package examples;
 import fun.Fun;
 
 /**
- * Example 3.1 from "Pizza into Java: Translating theory into practise"
- * by Martin Odersky and Philip Walder.
+   Example 3.1 from "Pizza into Java: Translating theory into practise"
+   by Martin Odersky and Philip Walder.
+
+class Radix {
+  int n = 0;
+  (char)->boolean radix(int r) {
+    return fun boolean(char c) {
+      n++; return '0' <= c && c < '0' + r;
+    }
+  }
+  String test () {
+    (char)->boolean f = radix(8);
+    return f('0')+" "+f('9')+" "+n;
+  }
+}
+
  */
 class Radix {
 
